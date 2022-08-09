5a09efa9da6ac24af03ec029a9a8a525cff9fbf0
Ankit-Saha-2003 <92501201+Ankit-Saha-2003@users.noreply.github.com>
2022-08-09 23:06:19 +0530

    Add code for 5.3, 5.5 and 6.5

--- Assignment_1/codes/5.5-toeplitz.py
+++ Assignment_1/codes/5.5-toeplitz.py
@@ -16,38 +16,10 @@ def x(n):
     else:
         return 6 - n
 
-def delta(n):
-    if n == 0:
-        return 1
-    else:
-        return 0
-
-def h(n):
-    if n == 0:
-        return 1
-    elif n > 0:
-        return delta(n) + delta(n-2) - 0.5*h(n-1)
-    else:
-        return 2*(delta(n+1) + delta(n-1) - h(n+1))
-
 vec_x = scipy.vectorize(x, otypes=[float])
-vec_h = scipy.vectorize(h, otypes=[float])
-
 N = np.linspace(0, 19, 20)
 x_array = vec_x(N)
-h_array = vec_h(N)
-
-m = h_array.shape[0]
-toeplitz_h = np.zeros((2 * m - 1, m))
-
-for i in range(m):
-    for j in range(i + 1):
-        toeplitz_h[i, j] = h_array[i - j]
-
-for i in range(m, 2 * m - 1):
-    for j in range(i - m + 1, m):
-        toeplitz_h[i, j] = h_array[i - j]
-
+toeplitz_h = np.loadtxt('toeplitz.dat', dtype='double')
 y_array = np.dot(toeplitz_h, x_array)
 
 plt.stem(N, y_array[:20])
